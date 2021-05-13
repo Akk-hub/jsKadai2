fc4c1e7d0fa17b2c97bc4a0d5fa983c6ce73913d
Akk-hub <72206144+Akk-hub@users.noreply.github.com>
2021-05-13 23:31:06 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,45 +1,45 @@
-function isFizz(number, n1) {
+const getFizz = (number, n1) => {
   if (number % n1 === 0) {
     return true;
   }
   return false;
-}
-function isBuzz(number, n2) {
+};
+const getBuzz = (number, n2) => {
   if (number % n2 === 0) {
     return true;
   }
   return false;
-}
+};
 
 const getNumber = () => {
-  const num1_text = document.getElementById("num1").value;
-  const num2_text = document.getElementById("num2").value;
+  const fizzText = document.getElementById('num1').value;
+  const buzzText = document.getElementById('num2').value;
 
-  const num1 = Number(num1_text);
-  const num2 = Number(num2_text);
+  const fizz = Number(fizzText);
+  const buzz = Number(buzzText);
 
-  const target = document.getElementById("parent");
-  target.innerHTML = "【出力】";
+  const target = document.getElementById('parent');
+  target.innerHTML = '【出力】';
 
-  if (isNaN(num1) || isNaN(num2)) {
-    const element = document.createElement("div");
+  if (isNaN(fizz) || isNaN(buzz)) {
+    const element = document.createElement('div');
     target.appendChild(element);
 
-    element.innerHTML = "整数値を入力してください。";
+    element.innerHTML = '整数値を入力してください。';
   } else {
     for (let i = 1; i < 100; i++) {
-      fz = isFizz(i, num1);
-      bz = isBuzz(i, num2);
+      fz = getFizz(i, fizz);
+      bz = getBuzz(i, buzz);
 
-      const element = document.createElement("div");
+      const element = document.createElement('div');
       target.appendChild(element);
 
       if (fz && bz) {
-        element.innerHTML = "FizzBuzz " + i;
+        element.innerHTML = 'FizzBuzz ' + i;
       } else if (fz) {
-        element.innerHTML = "Fizz " + i;
+        element.innerHTML = 'Fizz ' + i;
       } else if (bz) {
-        element.innerHTML = "Buzz " + i;
+        element.innerHTML = 'Buzz ' + i;
       }
     }
   }
